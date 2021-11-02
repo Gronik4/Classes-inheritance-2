cfd3a4eb550867b26d40d7990ea40b91636d3ed1
Gronik64 <golegni@yandex.ru>
2021-11-02 19:04:10 +0300

    Прототипы, наследование задача №2

--- src/js/Character.js
+++ src/js/Character.js
@@ -10,4 +10,21 @@ export default class Character {
     this.attack = 1;
     this.defence = 1;
   }
+
+  levelUp() {
+    if(this.health > 0) {
+      this.level += 1;
+      this.attack += this.attack * 0.2;
+      this.defence += this.defence * 0.2;
+      this.health = 100;
+    } else {
+      throw new Error('Нельзя повысить уровень умершего!');
+    }
+  }
+
+  damage(points) {
+    if(this.health > 0){
+      this.health -= points * (1 - this.defence / 100);
+    }
+  }
 }
--- src/js/__tests__/test.Character.js
+++ src/js/__tests__/test.Character.js
@@ -1,3 +1,4 @@
+import Character from '../Character';
 import Bowerman from '../Bowerman';
 import Swordsman from '../Swordsman';
 import Magician from '../Magician';
@@ -6,40 +7,59 @@ import Undead from '../Undead';
 import Zombie from '../Zombie';
 
 const expected = [
-  [['name', 'Ваня'], ['type', 'Bowman'], ['health', 100], ['level', 1], ['attack', 25], ['defence', 25]],
-  [['name', 'Ваня'], ['type', 'Swordsman'], ['health', 100], ['level', 1], ['attack', 40], ['defence', 10]],
-  [['name', 'Ваня'], ['type', 'Magician'], ['health', 100], ['level', 1], ['attack', 10], ['defence', 40]],
-  [['name', 'Ваня'], ['type', 'Daemon'], ['health', 100], ['level', 1], ['attack', 10], ['defence', 40]],
-  [['name', 'Ваня'], ['type', 'Undead'], ['health', 100], ['level', 1], ['attack', 25], ['defence', 25]],
-  [['name', 'Ваня'], ['type', 'Zombie'], ['health', 100], ['level', 1], ['attack', 40], ['defence', 10]],
+  { name: 'Ваня', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25 },
+  { name: 'Ваня', type: 'Swordsman', health: 100, level: 1, attack: 40, defence: 10 },
+  { name: 'Ваня', type: 'Magician', health: 100, level: 1, attack: 10, defence: 40 },
+  { name: 'Ваня', type: 'Daemon', health: 100, level: 1, attack: 10, defence: 40 },
+  { name: 'Ваня', type: 'Undead', health: 100, level: 1, attack: 25, defence: 25 },
+  { name: 'Ваня', type: 'Zombie', health: 100, level: 1, attack: 40, defence: 10 },
+  { name: 'Ваня', type: 'Bowman', health: 92.5, level: 1, attack: 25, defence: 25 },
 ];
 
 test('Test Bowerman', () => {
   const resalt = new Bowerman('Ваня', 'Bowman');
-  expect(Object.entries(resalt)).toEqual(expected[0]);
+  expect(resalt).toEqual(expected[0]);
 });
 
 test('Test Swordsman', () => {
   const resalt = new Swordsman('Ваня', 'Swordsman');
-  expect(Object.entries(resalt)).toEqual(expected[1]);
+  expect(resalt).toEqual(expected[1]);
 });
 
 test('Test Magician', () => {
   const resalt = new Magician('Ваня', 'Magician');
-  expect(Object.entries(resalt)).toEqual(expected[2]);
+  expect(resalt).toEqual(expected[2]);
 });
 
 test('Test Daemon', () => {
   const resalt = new Daemon('Ваня', 'Daemon');
-  expect(Object.entries(resalt)).toEqual(expected[3]);
+  expect(resalt).toEqual(expected[3]);
 });
 
 test('Test Undead', () => {
   const resalt = new Undead('Ваня', 'Undead');
-  expect(Object.entries(resalt)).toEqual(expected[4]);
+  expect(resalt).toEqual(expected[4]);
 });
 
 test('Test Zombie', () => {
   const resalt = new Zombie('Ваня', 'Zombie');
-  expect(Object.entries(resalt)).toEqual(expected[5]);
+  expect(resalt).toEqual(expected[5]);
+});
+
+test('Test Character on Error', () => {
+  expect(Bowerman).toThrow();
+});
+
+test('Test damage', () => {
+  let bowman = new Bowerman('Ваня', 'Bowman');
+  bowman.damage(10);
+  const resalt = bowman;
+  expect(resalt).toEqual(expected[6]);
+});
+
+test('Test levelUp', () => {
+  let bowman = new Character('Ваня', 'Bowman');
+  bowman.levelUp();
+  const resalt = bowman.level;
+  expect(resalt).toBe(2);
 });
